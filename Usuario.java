97eff9c13366a0c3e4727fb5c7274c9bc29f9961
MelMucino <133605372+MelMucino@users.noreply.github.com>
2023-05-23 08:40:43 -0600

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,10 @@
-
+import java.io.Serializable;
+public class Usuario implements Serializable {
+  private String nombre_completo;
+  private String nombre_usuario;
+  private String contraseña;
+  private int telefono;
+  private String correo;
+  private int prestamos;
+  private int tiempo_penalizacion;
+}
